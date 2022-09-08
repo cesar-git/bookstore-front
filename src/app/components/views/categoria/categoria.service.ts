65b3709417bc011e6e5afb206cf1dcadb04ac8b3
Cesar Cesar <cesarsax@gmail.com>
2022-09-07 22:22:28 -0300

    UPDATE para Categoria foi implementado

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -28,12 +28,17 @@ export class CategoriaService {
 
   create(categoria: Categoria): Observable<Categoria>{
     const url = `${this.baseUrl}/categorias`;
-    return this.http.get<Categoria>(url);
+    return this.http.post<Categoria>(url, categoria);
   }
 
   delete(id: String): Observable<void> {
     const url = `${this.baseUrl}/categorias/${id}`;
     return this.http.delete<void>(url);
+  }
+
+  update(categoria: Categoria): Observable<void> {
+    const url = `${this.baseUrl}/categorias/${categoria.id}`;
+    return this.http.put<void>(url, categoria);
   }
 
   mensagem(str: String): void {
